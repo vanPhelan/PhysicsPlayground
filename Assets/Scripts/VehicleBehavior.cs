edaeb4fcb5524dd6647e3a16f4a741c6490510b7
Van Phelan <vephelan@gmail.com>
2021-08-27 16:02:05 -0500

    Added some user input to the vehicle

---
+++ Assets/Scripts/VehicleBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleBehavior : MonoBehaviour
+{
+    public HingeJoint frontLeft;
+    public HingeJoint frontRight;
+    public HingeJoint rearLeft;
+    public HingeJoint rearRight;
+
+    private void Update()
+    {
+        JointMotor rearLeftMotor = rearLeft.motor;
+        rearLeftMotor.targetVelocity = 0.0f;
+        rearLeft.motor = rearLeftMotor;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rearLeftMotor.targetVelocity = 500.0f;
+            rearLeft.motor = rearLeftMotor;
+        }
+
+        JointMotor rearRightMotor = rearRight.motor;
+        rearRightMotor.targetVelocity = 0.0f;
+        rearLeft.motor = rearRightMotor;
+        if (Input.GetKey(KeyCode.D))
+        {
+            rearRightMotor.targetVelocity = -500.0f;
+            rearRight.motor = rearRightMotor;
+        }
+    }
+}
