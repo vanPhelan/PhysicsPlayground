44589dc2d35c31ab19319215c78bd9f2367baca4
Van Phelan <vephelan@gmail.com>
2021-08-31 15:51:25 -0500

    Made player control relative to the camera

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,13 +6,17 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpStrength = 10.0f;
+    public float airControl = 1.0f;
     public float gravityModifier = 1.0f;
 
+    public Camera playerCamera;
+    
     private CharacterController _controller;
 
     private Vector3 _desiredVelocity;
     private Vector3 _airVelocity;
-    private bool _isJumpDesired;
+    private bool _isJumpDesired = false;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
@@ -26,6 +30,15 @@ public class PlayerBehavior : MonoBehaviour
         _desiredVelocity.y = 0.0f;
         _desiredVelocity.z = Input.GetAxis("Vertical");
 
+        //Get camera forward
+        Vector3 cameraForward = playerCamera.transform.forward;
+        cameraForward.y = 0.0f;
+        cameraForward.Normalize();
+        //Get camera right
+        Vector3 cameraRight = playerCamera.transform.right;
+
+        _desiredVelocity = _desiredVelocity.x * cameraRight + _desiredVelocity.z * cameraForward;
+
         //Get jump input
         _isJumpDesired = Input.GetButtonDown("Jump");
 
@@ -33,16 +46,26 @@ public class PlayerBehavior : MonoBehaviour
         _desiredVelocity.Normalize();
         _desiredVelocity *= speed;
 
+        //Check for ground
+        _isGrounded = _controller.isGrounded;
+
         //Apply jump strength
-        if (_isJumpDesired)
+        if (_isJumpDesired && _isGrounded)
         {
             _airVelocity.y = jumpStrength;
             _isJumpDesired = false;
         }
 
+        //Stop on ground
+        if (_isGrounded && _airVelocity.y < 0.0f)
+        {
+            _airVelocity.y = -1.0f;
+        }
+
         //Apply gravity
         _airVelocity += Physics.gravity * gravityModifier * Time.deltaTime;
 
+        //Add air velocity
         _desiredVelocity += _airVelocity;
 
         //Move
